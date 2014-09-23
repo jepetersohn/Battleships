05452ff70b79ff719ecc3d372fa39e0263f30e13
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-23 15:12:28 +0100

    Removed default values for ship args

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,50 +1,41 @@
 class Ship
 
-	def initialize(size=2, name=:ship, hits=0)
+		attr_reader :size,:name
+		attr_accessor :hit_count
+
+	def initialize(size, name, hit_count)
 		@size = size
 		@name = name
-		@hits = hits
-	end
-
-	def size
-		@size
-	end
-
-	def name
-		@name
-	end
-
-	def hit_count
-		@hits
+		@hit_count = hit_count
 	end
 
 	def receive_shot
-		raise "This ship can't be hit again(its sunk!)" if @hits >= @size
-		@hits += 1
+		raise "This ship can't be hit again(its sunk!)" if @hit_count >= @size
+		@hit_count += 1
 	end
 
 	def sunk?
-		hit_count == size ? true : false
+		@hit_count == size ? true : false
 	end
 
 	def self.aircraftcarrier
-		new(5, :aircraftcarrier)
+		new(5, :aircraftcarrier, 0)
 	end
 
 	def self.battleship
-		new(4, :battleship)
+		new(4, :battleship, 0)
 	end
 	
 	def self.submarine
-		new(3, :submarine)
+		new(3, :submarine, 0)
 	end	
 
 	def self.destroyer
-		new(3, :destroyer)
+		new(3, :destroyer, 0)
 	end
 
 	def self.patrolboat
-		new(2, :patrolboat)
+		new(2, :patrolboat, 0)
 	end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,67 +2,71 @@ require 'ship'
 
 describe Ship do
 
-	let (:ship) {Ship.new}
+	let (:ship) {Ship.new(2, :ship, 0)}
 	let (:aircraftcarrier) {Ship.aircraftcarrier}
 	let (:battleship) {Ship.battleship}
 	let (:submarine) {Ship.submarine}
 	let (:destroyer) {Ship.destroyer} 
 	let (:patrolboat) {Ship.patrolboat}
 
-	it "should have a size" do
-		expect(ship.size).not_to eq(0)
+	context "On initialize a ship" do
+
+		it "should have a size" do
+			expect(ship.size).not_to eq(0)
+		end
+
+		it "should have a name" do
+			expect(ship.name).to be(:ship)
+		end
+
+		it "has not hit" do
+			expect(ship.hit_count).to eq(0)
+
+		end
+
+		it "is not sunk" do
+			expect(ship).not_to be_sunk
+		end
 	end
 
-	it "should have a name" do
-		expect(ship.name).to be(:ship)
+	context "During the game a ship" do
+
+		it "can take a hit" do
+			expect{ship.receive_shot}.to change{ship.hit_count}.by 1
+		end
+
+		it "can't take more hits than its size" do
+			2.times{ship.receive_shot}
+			expect{ship.receive_shot}.to raise_error(RuntimeError)
+		end
+	
+
+		it "can be sunk if hits equal its size" do
+			2.times{ship.receive_shot}
+			expect(ship).to be_sunk
+		end
 	end
 
-	it "has not hit" do
-		expect(ship.hit_count).to eq(0)
+	context "Different types of ships" do	
 
+		it "has a size of 5 if it is an aircraftcarrier" do
+			expect(aircraftcarrier.size).to eq(5)
+		end
+
+		it "has a size of 4 if it is a battleship" do
+			expect(battleship.size).to eq(4)
+		end
+
+		it "has a size of 3 if it is a submarine" do
+			expect(submarine.size).to eq(3)
+		end
+
+		it "has a size of 3 if it is a destroyer" do
+			expect(destroyer.size).to eq(3)
+		end
+
+		it "has a size of 2 if it is a patrol boat" do
+			expect(patrolboat.size).to eq(2)
+		end
 	end
-
-	it "can take a hit" do
-		expect{ship.receive_shot}.to change{ship.hit_count}.by 1
-	end
-
-	it "can't take more hits than its size" do
-		ship = Ship.new(2)
-		2.times{ship.receive_shot}
-		expect{ship.receive_shot}.to raise_error(RuntimeError)
-	end
-
-	it "is not sunk" do
-		expect(ship).not_to be_sunk
-	end
-
-
-	it "can be sunk if hits equal its size" do
-		ship = Ship.new(2)
-		2.times{ship.receive_shot}
-		expect(ship).to be_sunk
-	end
-
-	it "has a size of 5 if it is an aircraftcarrier" do
-		expect(aircraftcarrier.size).to eq(5)
-	end
-
-	it "has a size of 4 if it is a battleship" do
-		expect(battleship.size).to eq(4)
-	end
-
-	it "has a size of 3 if it is a submarine" do
-		expect(submarine.size).to eq(3)
-	end
-
-	it "has a size of 3 if it is a destroyer" do
-		expect(destroyer.size).to eq(3)
-	end
-
-	it "has a size of 2 if it is a patrol boat" do
-		expect(patrolboat.size).to eq(2)
-	end
-
-	it "should report "
-
 end
