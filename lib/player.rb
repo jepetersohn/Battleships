c8a55e1badafe1bae00a63dd0b2235ebc1a314a5
Nicole Pellicena Tredway <pellicena@gmail.com>
2014-09-24 13:42:52 +0100

    Player can place part of ship and shoot

--- lib/player.rb
+++ lib/player.rb
@@ -1,26 +1,29 @@
 class Player
 
-	def initialize
-		@primary_board = "PrimaryBoard.new"
-		@tracking_board = "TrackingBoard.new"
+	def initialize(board: :board_substitute)
+		@own_board = board
 		@ships = [:patrolboat, :battleship]
 	end
 
-	def primary_board?
-		@primary_board
-	end
-
-	def tracking_board?
-		@tracking_board
+	def own_board?
+		@own_board
 	end
 
 	def ships
 		@ships
 	end
 
-	def place_ship(ship)
-		# ship.add(@primary_board)
+	def place_ship(own_board, at_coordinate, ship)
+		own_board.grid[at_coordinate].ship(ship)
 		@ships.delete(ship)
 	end
 
+	# method to loop place_ship method * ship length
+
+	def shoot_at(opponent_board, at_coordinate)
+		opponent_board.grid[at_coordinate].incoming_shot
+	end 
+
 end
+
+
