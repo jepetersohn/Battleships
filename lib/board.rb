bb7d19eb0f392f376e012b648cc7fa4678a6153f
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-25 11:52:31 +0100

    Board can convert coordinates and place ships

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,7 @@
 class Board
 
+	COLUMNS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
 	# attr_reader =	:grid
 
 	def initialize
@@ -9,7 +11,30 @@ class Board
 	def grid
 		@grid
 	end
-		
+	
+	def place_ship(ship, coordinates, direction)
+		cells_to_fill = ship.size
+		column, row = coord_check(coordinates)
+		while cells_to_fill > 0
+			if direction == "R"
+				grid[column][row].ship = ship
+				grid[column][row].status = :occupied
+				column += 1
+			end
+			if direction == "D"
+				grid[column][row].ship = ship
+				grid[column][row].status = :occupied
+				row += 1
+			end
+			cells_to_fill -= 1
+	end
+	end
+
+	def coord_check(coordinates)
+		coordinates = coordinates.scan(/\d+|\D+/)
+		return COLUMNS.index(coordinates[0]), coordinates[1].to_i - 1
+	end
+
 end
 
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,6 +4,9 @@ describe Board do
 
 	let(:board) {Board.new}
 	let(:cell) {double :cell}
+	let(:ship) {double :ship}
+	let(:patrolboat) {double :patrolboat, size: 2 }
+	let(:battleship) {double :battleship, size: 4 }
 
 	context "Upon initialize it" do
 
@@ -24,7 +27,31 @@ describe Board do
 		end
 	end	
 
+	context "During the game it" do
+
+		it "should know how to convert coordinates" do
+			expect(board.coord_check("A1")).to eq [0, 0]
+		end
 
 
-	
+
+		it "should be able to receive ship starting coordinate" do
+			board.place_ship(patrolboat, "A1", "R")
+			expect(board.grid[0][0].ship).to eq(patrolboat)
+		end
+
+		it "should be able to place patrolboat coordinates" do
+			board.place_ship(patrolboat, "A1", "R")
+			expect(board.grid[0][0].ship).to eq(patrolboat)
+			expect(board.grid[1][0].ship).to eq(patrolboat)
+		end
+
+		it "should be able to place battleship coordinates" do
+			board.place_ship(battleship, "A1", "D")
+			expect(board.grid[0][0].ship).to eq(battleship)
+			expect(board.grid[0][1].ship).to eq(battleship)
+			expect(board.grid[0][2].ship).to eq(battleship)
+			expect(board.grid[0][3].ship).to eq(battleship)
+		end	
+	end
 end
