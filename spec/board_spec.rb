e3e773a9080a0a1693592ccc30530a93f249c962
Andrew Snead <andrewcsnead@gmail.com>
2014-09-25 18:42:53 +0100

    Add board spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -33,8 +33,6 @@ describe Board do
 			expect(board.coord_converter("A1")).to eq [0, 0]
 		end
 
-
-
 		it "should be able to receive ship starting coordinate" do
 			board.place_ship(patrolboat, "A1", "R")
 			expect(board.grid[0][0].ship).to eq(patrolboat)
@@ -52,6 +50,27 @@ describe Board do
 			expect(board.grid[0][1].ship).to eq(battleship)
 			expect(board.grid[0][2].ship).to eq(battleship)
 			expect(board.grid[0][3].ship).to eq(battleship)
-		end	
+		end
+
+		it "should be able to verify if ship will not fit on board" do
+			expect(board.board_fit?(battleship, "A8", "D")).to be false
+			expect(board.board_fit?(battleship, "I1", "R")).to be false
+		 end
+
+		it "should be able to verify is ship will fit on board" do
+			expect(board.board_fit?(battleship, "A7", "D")).to be true
+			expect(board.board_fit?(battleship, "G1", "R")).to be true
+		end
+
+		it "should be able to check if ship placement will clash" do
+			board.place_ship(battleship, "A1", "R")
+			expect(board.ship_clash?(battleship, "D1", "D")).to be true
+			expect(board.ship_clash?(battleship, "E1", "D")).to be false
+		end
+
+		it "should be able to shoot at a coordinate" do
+			allow(board.grid[2][3]).to receive(:incoming_shot).and_return(:hit)
+			expect(board.shoot_at("C4")).to eq(:hit)
+		end
 	end
 end
