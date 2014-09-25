cec24c97cf462b3aad3594fb539f8796c79c3aef
Andrew Snead <andrewcsnead@gmail.com>
2014-09-25 18:41:36 +0100

    add check methods for board

--- lib/board.rb
+++ lib/board.rb
@@ -1,32 +1,50 @@
 class Board
 
-	attr_reader :grid
+  attr_reader :grid
 
-	def initialize
-		@grid = Array.new(10).map! {Array.new(10).map! {Cell.new}}
-	end	
+  DIMENSION = 10
 
-  def place_ship(ship, coordinates, direction)
-      x, y = coord_converter(coordinates)
+  def initialize
+    @grid = Array.new(DIMENSION).map! {Array.new(DIMENSION).map! {Cell.new}}
+  end 
+
+  def place_ship(ship, coordinate, direction)
+      x, y = coord_converter(coordinate)
       ship.size.times do 
-        put_ship_on_grid(ship, x, y )
+        put_ship_on_grid(ship, x, y)
         direction == "R" ? x += 1 : y += 1
       end
   end
 
-  def put_ship_on_grid(ship,x,y )
+  def put_ship_on_grid(ship, x, y)
     grid[x][y].ship = ship
     # grid[x][y].status = :occupied
   end
-
-	def coord_converter(coordinates)	
-    [coordinates.split('', 2)[0].upcase.ord - 65, coordinates.split('', 2)[1].to_i - 1]
-	end
 
   def shoot_at(coordinates)
     x,y = coord_converter(coordinates)
     grid[x][y].incoming_shot
   end
+
+  def coord_converter(coordinate) 
+    [coordinate.split('', 2)[0].upcase.ord - 65, coordinate.split('', 2)[1].to_i - 1]
+  end
+
+  def board_fit?(ship, coordinate, direction)
+    x, y = coord_converter(coordinate)
+    return false if direction == "R" && (ship.size + x) > DIMENSION
+    return false if direction == "D" && (ship.size + y) > DIMENSION
+    true
+  end
+
+  def ship_clash? (ship, coordinate, direction)
+    x, y = coord_converter(coordinate)
+    ship.size.times do
+      return true if grid[x][y].ship
+      direction == "R" ? x += 1 : y += 1
+    end
+    false
+  end
+
+
 end
-
-
