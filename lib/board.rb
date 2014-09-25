47db3166b8a0498be1981d9ed84c917bda1f1d01
Andrew Snead <andrewcsnead@gmail.com>
2014-09-25 14:31:18 +0100

    refactor coord convert and place ship

--- lib/board.rb
+++ lib/board.rb
@@ -1,38 +1,26 @@
 class Board
 
-	COLUMNS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-
-	# attr_reader =	:grid
+	attr_reader :grid
 
 	def initialize
 		@grid = Array.new(10).map! {Array.new(10).map! {Cell.new}}
 	end	
 
-	def grid
-		@grid
-	end
-	
-	def place_ship(ship, coordinates, direction)
-		cells_to_fill = ship.size
-		column, row = coord_check(coordinates)
-		while cells_to_fill > 0
-			if direction == "R"
-				grid[column][row].ship = ship
-				grid[column][row].status = :occupied
-				column += 1
-			end
-			if direction == "D"
-				grid[column][row].ship = ship
-				grid[column][row].status = :occupied
-				row += 1
-			end
-			cells_to_fill -= 1
-	end
-	end
+  def place_ship(ship, coordinates, direction)
+      x, y = coord_converter(coordinates)
+      ship.size.times do 
+        put_ship_on_grid(ship, x, y )
+        direction == "R" ? x += 1 : y += 1
+      end
+  end
 
-	def coord_check(coordinates)
-		coordinates = coordinates.scan(/\d+|\D+/)
-		return COLUMNS.index(coordinates[0]), coordinates[1].to_i - 1
+  def put_ship_on_grid(ship,x,y )
+    grid[x][y].ship = ship
+    grid[x][y].status = :occupied
+  end
+
+	def coord_converter(coordinates)	
+    [coordinates.split('', 2)[0].upcase.ord - 65, coordinates.split('', 2)[1].to_i - 1]
 	end
 
 end
